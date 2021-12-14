8cbce00e645b70d9baafecb5fb777265597d130e
samamstar <samamstar@gmail.com>
2021-12-13 17:09:18 -0700

    Switched to full header dump source
    Thanks @Buckminsterfullerene02

--- DRGLib UE project/Source/FSD/FSDGameUserSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "FSDGameUserSettings.h"
-
-UFSDGameUserSettings* UFSDGameUserSettings::GetFSDGameUserSettings() 
-{
-	return nullptr;
-}
