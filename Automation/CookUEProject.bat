fc2b852ff66aa8884cd8b146bc99c72a3d404402
samamstar <samamstar@gmail.com>
2022-03-19 19:09:02 -0600

    Updated CookUEProject.bat to recognize cook errors

--- Automation/CookUEProject.bat
+++ Automation/CookUEProject.bat
@@ -26,6 +26,13 @@ echo cooking UE project
 
 "%UnrealEngineLocation%\Engine\Binaries\Win64\UE4Editor-Cmd.exe" "%ProjectFolder%%ProjectFile%" -run=cook -targetplatform=WindowsNoEditor
 
+if %errorlevel% gtr 0 (
+	echo Error reported by UE! Stopping bats
+	echo 
+	pause
+	exit
+)
+
 echo cooking complete
 
 ::There's probably a better way to pass an argument to disable pausing, but Oh well
