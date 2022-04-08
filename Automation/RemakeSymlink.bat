fb0c7f90aab21236dbf7c9c408a3aadf49fcd43e
samamstar <samamstar@gmail.com>
2022-04-08 09:55:34 -0600

    4.2.1 (#33)
    - LIB_WT_InputKeySelector
      - [N] Fixed bug where it wouldn't print overlapping custom inputs properly in AllowOverlaps mode
    - LIB_F_Input
      - [N] Fixed bug where remapping custom input wouldn't properly update custom input list
    - LIB_DEB_W_HudProfiles
      - [+] added button that creates an empty profile
    - LIB_W_LogSettingsPage
      - [N] Improved performance of logging by updating log output widget by having it only update while visible
    
    
    * Switched to relative paths for ModHub symlink
    
    * Fixed bug where remapping keys wouldn't update active custom input list
    
    * Improved performance of logging by making "RealTime" log output widget only update when visible and once per second
    
    * Added button to add empty debug profile
    
    * Fixed bug where IKS wouldn't properly print custom inputs when in AllowOverlaps mode
    
    * Fixed bug where Overlay page IKS's wouldn't initialize because they spawned too early

--- Automation/RemakeSymlink.bat
+++ Automation/RemakeSymlink.bat
@@ -14,9 +14,9 @@ rem change the current directory to the bat location
 pushd %~dp0
 
 echo making symbolic link
-cd ..
-rmdir "%cd%\DRGLib UE project\Content\ModHub"
-del "%cd%\DRGLib UE project\Content\ModHub"
-mklink /d "%cd%\DRGLib UE project\Content\ModHub" "%cd%\ModHubModule\FSD\Content\ModHub"
+cd "..\DRGLib UE project\Content"
+rmdir ModHub"
+del "ModHub"
+mklink /d "ModHub" "..\..\ModHubModule\FSD\Content\ModHub"
 
 pause
