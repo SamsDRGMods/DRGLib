3fde03412929940dab039f0629f21f1a603767e2
samamstar <samamstar@gmail.com>
2022-07-01 21:01:59 -0600

    Re-added logic to simultaneously package TestMod and DRGLib

--- Automation/TestModAutomation/QuickTestMod.bat
+++ Automation/TestModAutomation/QuickTestMod.bat
@@ -20,4 +20,22 @@ del "%SteamInstall%\FSD\Mods\%ModName%\*" /q
 echo copying over new mod pak
 move "%cd%\Temp\%ModName%.pak" "%SteamInstall%\FSD\Mods\%ModName%\"
 
+rem Package DRGLib stuff
+
+cd ..
+
+call UtilityBats/MakeDefaultConfigFiles.bat --noPause
+call UtilityBats/LoadVars.bat
+call UtilityBats/VerifyVars.bat --noPause
+
+call UtilityBats/PackageMod.bat --noPause
+
+mkdir "%SteamInstall%\FSD\Mods\%ModName%"
+
+echo removing old mod pak
+del "%SteamInstall%\FSD\Mods\%ModName%\*" /q
+
+echo copying over new mod pak
+move "%cd%\Temp\%ModName%.pak" "%SteamInstall%\FSD\Mods\%ModName%\"
+
 start steam://rungameid/548430
